3c199b1b52d86ecf6ce4c2259fdbad83ca34d2d8
zhuchunyao <zhuchunyao@haier.com>
2020-08-31 15:58:09 +0800

    update build.gradle.

--- app/src/main/java/com/zcy/videoplayer/StartActivity.java
+++ app/src/main/java/com/zcy/videoplayer/StartActivity.java
@@ -5,8 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-import com.zcy.player.BuildConfig;
-import com.zcy.player.DefaultPlayerManager;
+import com.zcy.player.player.IjkPlayerManager;
 import com.zcy.player.player.PlayerFactory;
 import com.zcy.player.utils.VideoType;
 
@@ -15,10 +14,9 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        PlayerFactory.setPlayManager(DefaultPlayerManager.class);
+        PlayerFactory.setPlayManager(IjkPlayerManager.class);
         VideoType.setShowType(VideoType.SCREEN_TYPE_DEFAULT);
         VideoType.setRenderType(VideoType.TEXTURE);
-        PlayerFactory.debug(BuildConfig.DEBUG);
         setContentView(R.layout.activity_start);
         findViewById(R.id.start).setOnClickListener(view -> {
             startActivity(new Intent(StartActivity.this, MainActivity.class));
